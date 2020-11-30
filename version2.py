34b65f46f730be6b2d73686b477ff4ca31a5846e
Patrick <huangpatrick16777216@gmail.com>
2020-11-30 09:48:20 -0800

    Uci loop.

---
+++ version2.py
@@ -0,0 +1,56 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import chess
+
+
+def main():
+    position = chess.Board()
+
+    while True:
+        msg = input().strip()
+        
+        if msg == "quit":
+            return
+        elif msg == "isready":
+            print("readyok", flush=True)
+        elif msg == "uci":
+            print("uciok", flush=True)
+        elif msg == "d":
+            print(position)
+
+        elif msg == "ucinewgame":
+            position = chess.Board()
+        elif msg.startswith("position"):
+            msg = msg.replace("position", "").strip()
+            if msg.startswith("startpos"):
+                msg = msg.replace("startpos", "").strip()
+                position = chess.Board()
+                if msg.startswith("moves"):
+                    moves = msg.replace("moves", "").strip().split(" ")
+                    for move in moves:
+                        position.push_uci(move)
+
+            elif msg.startswith("fen"):
+                fen = msg.replace("fen", "").strip()
+                position = chess.Board(fen)
+
+        elif msg.startswith("go"):
+            print("going")
+
+
+main()

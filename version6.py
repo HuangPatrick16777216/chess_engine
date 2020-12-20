9d9f3a316d5522926b07623edc94625a7efac0a6
Patrick Huang <huangpatrick16777216@gmail.com>
2020-12-20 10:49:15 -0800

    Copied uci loop.

---
+++ version6.py
@@ -0,0 +1,70 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import chess
+import threading
+
+
+def main():
+    position = chess.Board()
+
+    while True:
+        msg = input().strip()
+
+        if msg == "quit":
+            return
+        elif msg == "isready":
+            print("readyok", flush=True)
+        elif msg == "uci":
+            print("uciok", flush=True)
+        elif msg == "d":
+            print(position, flush=True)
+
+        elif msg == "ucinewgame":
+            position = chess.Board()
+        elif msg.startswith("position"):
+            msg = msg.replace("position", "").strip()
+            if msg.startswith("startpos"):
+                msg = msg.replace("startpos", "").strip()
+                position = chess.Board()
+                if msg.startswith("moves"):
+                    moves = msg.replace("moves", "").strip().split(" ")
+                    for move in moves:
+                        position.push_uci(move)
+
+            elif msg.startswith("fen"):
+                fen = msg.replace("fen", "").strip()
+                position = chess.Board(fen)
+
+        elif msg.startswith("go"):
+            msg = msg.replace("go", "").strip()
+            kwargs = {"position": position}
+
+            if msg.startswith("nodes"):
+                kwargs["nodes"] = int(msg.replace("nodes", "").strip())
+            elif msg.startswith("depth"):
+                kwargs["depth"] = int(msg.replace("depth", "").strip())
+            elif msg.startswith("movetime"):
+                kwargs["movetime"] = int(msg.replace("movetime", "").strip())
+            elif "wtime" in msg or "btime" in msg:
+                parts = msg.split(" ")
+                for i in range(0, len(parts)//2):
+                    kwargs[parts[i*2]] = int(parts[i*2+1]) / 1000
+
+            threading.Thread(target=tree.search, kwargs=kwargs).start()
+        elif msg == "stop":
+            tree.active = False

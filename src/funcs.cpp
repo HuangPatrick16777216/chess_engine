cea9657b86fe192ce42ce849b309cb2dc9d33a13
Patrick Huang <huangpatrick16777216@gmail.com>
2020-12-30 10:33:52 -0800

    Indented gpl disclaimer.

--- src/funcs.cpp
+++ src/funcs.cpp
@@ -1,19 +1,19 @@
 /*
-  A UCI compatible Chess engine.
-  Copyright (C) 2020 HuangPatrick16777216
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    A UCI compatible Chess engine.
+    Copyright (C) 2020 HuangPatrick16777216
+  
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <iostream>
@@ -28,4 +28,9 @@ string strip(string str) {
     start = str.find_first_not_of(" ");
     end = str.find_last_not_of(" ");
     return str.substr(start, end-start+1);
+}
+
+
+vector<string> split(string str) {
+
 }

43a7d0e8cf39504eadafc1569a1a1f53647ad504
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-21 13:01:25 -0800

    Fixed minimax bugs.

--- src/tree.py
+++ src/tree.py
@@ -39,10 +39,10 @@ class Tree:
 
     def go(self, **kwargs):
         self.init_vars(kwargs["board"])
-        self.root = Node(kwargs["board"], 0, self)
+        self.root = Node(kwargs["board"], 0, self, None)
         threading.Thread(target=self.printer).start()
 
-        for depth in range(4):
+        for depth in range(5):
             self.depth = depth
             self.root.branch(depth)
 
@@ -62,16 +62,21 @@ class Tree:
 
 
 class Node:
-    def __init__(self, board: chess.Board, depth, tree: Tree):
+    def __init__(self, board: chess.Board, depth, tree: Tree, last_move):
         self.board = board
         self.depth = depth
         self.tree = tree
         self.children = []
+        self.last_move = last_move
 
         self.eval = evaluate(board)
         self.move = None
+        self.info = None
+        self.info_dirty = True
 
     def branch(self, target_depth):
+        self.info_dirty = True
+
         if target_depth == self.depth+1:
             for move in self.board.generate_legal_moves():
                 if not self.tree.active:
@@ -79,10 +84,12 @@ class Node:
 
                 new_board = deepcopy(self.board)
                 new_board.push(move)
-                new_node = Node(self.board, self.depth+1, self.tree)
+                new_node = Node(self.board, self.depth+1, self.tree, move)
 
                 self.children.append(new_node)
                 self.tree.nodes += 1
+
+            self.minimax()
 
         elif target_depth > self.depth+1:
             for c in self.children:
@@ -90,21 +97,31 @@ class Node:
                     return
                 c.branch(target_depth)
 
+            self.minimax()
+
     def minimax(self):
+        if not self.info_dirty:
+            return self.info
+
         if len(self.children) == 0:
-            return (self.eval, self.board.peek())
+            move = self.last_move
+            self.info = (self.eval, move)
+            return self.info
 
         if self.board.turn:
             best_ind = 0
             for i, c in enumerate(self.children):
-                if c.eval > self.children[best_ind].eval:
+                if c.eval > self.children[best_ind].minimax()[0]:
                     best_ind = i
 
-            best_eval = self.children[best_ind].eval
-            best_move = self.children[best_ind].board.peek()
-            self.eval = best_eval
-            self.move = best_move
-            return (best_eval, best_move)
+            best = self.children[best_ind]
+            self.eval = best.minimax()[0]
+            self.move = best.last_move
+
+            self.info = (self.eval, self.move)
+            self.info_dirty = False
+
+            return self.info
 
         else:
             best_ind = 0
@@ -112,8 +129,11 @@ class Node:
                 if c.eval < self.children[best_ind].eval:
                     best_ind = i
 
-            best_eval = self.children[best_ind].eval
-            best_move = self.children[best_ind].board.peek()
-            self.eval = best_eval
-            self.move = best_move
-            return (best_eval, best_move)
+            best = self.children[best_ind]
+            self.eval = best.minimax()[0]
+            self.move = best.last_move
+
+            self.info = (self.eval, self.move)
+            self.info_dirty = False
+
+            return self.info

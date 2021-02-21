8fa9df76623d2e635ded85d622ce2249fdc7aafb
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-21 12:17:35 -0800

    Node branch.

--- src/tree.py
+++ src/tree.py
@@ -17,7 +17,9 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+import time
 import chess
+from copy import deepcopy
 
 
 class Tree:
@@ -27,7 +29,29 @@ class Tree:
     def init_vars(self, board):
         self.active = False
         self.board = board
+        self.nodes = 0
+        self.time_start = time.time()
 
     def go(self, **kwargs):
         self.init_vars(kwargs["board"])
-        print("bestmove e2e4", flush=True)
+        self.root = Node(kwargs["board"], 0, self)
+        for depth in range(4):
+            self.root.branch(depth)
+
+
+class Node:
+    def __init__(self, board, depth, tree):
+        self.board = board
+        self.depth = depth
+        self.tree = tree
+        self.children = []
+
+    def branch(self, target_depth):
+        if self.depth+1 == target_depth:
+            for move in self.board.generate_legal_moves():
+                new_board = deepcopy(self.board)
+                new_board.push(move)
+                new_node = Node(self.board, self.depth+1, self.tree)
+
+                self.children.append(new_node)
+                self.tree.nodes += 1
